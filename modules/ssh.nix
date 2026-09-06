795361259b728c2b5d8468a2192eea1f725a9ee0
Michail Augaitis <michael.aug@proton.me>
2026-09-06 18:23:36 +0100

    update ssh config

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,5 +1,21 @@
-{ ... }:
+{ username, ... }:
 
 {
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+
+    authorizedKeysFiles = [
+      "/home/${username}/.ssh/authorized_keys"
+    ];
+
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+      KbdInteractiveAuthentication = false;
+      ClientAliveCountMax = 1;
+
+      # Don't reveal unnecessary information
+      VersionAddendum = null;
+    };
+  };
 }
